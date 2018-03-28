07e83dcaa510a802ef76fde7f3c9cf0f43ea6fac
anish-agnihotri <contact@anishagnihotri.com>
2018-03-28 12:34:42 -0400

    Adding comments to cleanup code

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage();
+//Import fullpapge libraries. 
+$(document).ready(function () {
+    $('#fullpage').fullpage();
 });
+//Assign fullpaper anchors to sections. 
 $('#fullpage').fullpage({
     anchors: ['someanchor', 'landinganchor', 'servicesanchor', 'workanchor', 'contactanchor']
 });
-$(document).ready(function(){
+//Add hovering (with colour changes) to images in the workanchor. 
+$(document).ready(function () {
     $(".WorkOne").hover(
-        function() {$(this).attr("src","images/docker_light.svg");},
-        function() {$(this).attr("src","images/docker_dark.svg");
-    });
+        function () {
+            $(this).attr("src", "images/docker_light.svg");
+        },
+        function () {
+            $(this).attr("src", "images/docker_dark.svg");
+        });
     $(".WorkTwo").hover(
-        function() {$(this).attr("src","images/namecheap_light.svg");},
-        function() {$(this).attr("src","images/namecheap_dark.svg");
-    });
+        function () {
+            $(this).attr("src", "images/namecheap_light.svg");
+        },
+        function () {
+            $(this).attr("src", "images/namecheap_dark.svg");
+        });
     $(".WorkThree").hover(
-        function() {$(this).attr("src","images/shopify_light.svg");},
-        function() {$(this).attr("src","images/shopify_dark.svg");
-    });
+        function () {
+            $(this).attr("src", "images/shopify_light.svg");
+        },
+        function () {
+            $(this).attr("src", "images/shopify_dark.svg");
+        });
     $(".WorkFour").hover(
-        function() {$(this).attr("src","images/wordpress_light.svg");},
-        function() {$(this).attr("src","images/wordpress_dark.svg");
-    });
+        function () {
+            $(this).attr("src", "images/wordpress_light.svg");
+        },
+        function () {
+            $(this).attr("src", "images/wordpress_dark.svg");
+        });
 });
